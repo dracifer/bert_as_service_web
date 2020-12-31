da5fbb797b60b916954fd5fc8a0b54d0c2fb5e5c
dracier <dracifer@gmail.com>
2020-12-30 17:09:19 -0800

    add logging

--- build.sh
+++ build.sh
@@ -2,12 +2,17 @@
 # exit on error
 set -o errexit
 
+echo "Install poetry"
 poetry install
+echo "Finish poetry install"
 
 if [ ! -d backend/model/uncased_L-12_H-768_A-12 ];then
+    echo "downloading model"
     mkdir -p backend/model
     wget https://storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
     unzip uncased_L-12_H-768_A-12.zip -d backend/model/
+    rm uncased_L-12_H-768_A-12.zip
+    echo "done downloading model"
 fi
 
 #poetry shell
