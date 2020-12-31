df798bcfafb262ae82ad9ac2ff026ff017dee6c2
dracier <dracifer@gmail.com>
2020-12-30 17:09:19 -0800

    update

--- web/bert_sim_main/settings.py
+++ web/bert_sim_main/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 # Don't forget to import dj-database-url at the beginning of the file
 import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,13 +82,11 @@ WSGI_APPLICATION = 'bert_sim_main.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        dj_database_url.config(
-            # Feel free to alter this value to suit your needs.
-            default='postgresql://postgres:postgres@localhost:5432/bert_sim_main',
-            conn_max_age=600
-        )
-    }
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/bert_sim_main',
+        conn_max_age=600
+    ),
 }
 
 
